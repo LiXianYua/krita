9c48a4db6f7900f7347f9527af91a17511d8fc66
Digw <7836227+LiXianYua@users.noreply.github.com>
2026-08-29 04:00:04 -0700

    S-09-c: migrate render pass flags carrier

--- libs/image/KisRenderPassFlags.h
+++ libs/image/KisRenderPassFlags.h
@@ -6,12 +6,14 @@
 #ifndef KISRENDERPASSFLAGS_H
 #define KISRENDERPASSFLAGS_H
 
+#include <PkFlags.h>
+
 enum class KisRenderPassFlag
 {
     None = 0x0,
     NoTransformMaskUpdates = 0x1
 };
-Q_DECLARE_FLAGS(KisRenderPassFlags, KisRenderPassFlag)
-Q_DECLARE_OPERATORS_FOR_FLAGS(KisRenderPassFlags)
+PK_DECLARE_FLAGS(KisRenderPassFlags, KisRenderPassFlag)
+PK_DECLARE_OPERATORS_FOR_FLAGS(KisRenderPassFlags)
 
 #endif // KISRENDERPASSFLAGS_H
